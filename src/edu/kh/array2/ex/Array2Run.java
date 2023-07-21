8536069beb87e1e9a13c749cd82e19c45176f33a
aspyn1 <asypnisgood@gamil.com>
2023-07-21 17:36:58 +0900

    first commit

---
+++ src/edu/kh/array2/ex/Array2Run.java
@@ -0,0 +1,19 @@
+package edu.kh.array2.ex;
+
+public class Array2Run {
+
+	public static void main(String[] args) {
+
+		Array2Example Arr2Ex = new Array2Example();
+		
+		//Arr2Ex.ex1();
+		//Arr2Ex.ex2();
+		//Arr2Ex.ex3();
+		//Arr2Ex.ex4();
+		Arr2Ex.ex5();
+		
+		
+		
+	}
+
+}
